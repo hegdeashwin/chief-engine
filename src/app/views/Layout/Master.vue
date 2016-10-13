f0a56bad5adb7a38d1d6be578bcd29f5ae796366
hegdeashwin <ashwin.hegde3@gmail.com>
2016-10-14 01:12:02 +0530

    Add initial client side stack

---
+++ src/app/views/Layout/Master.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <HeaderArea></HeaderArea>
+    <div class="container">
+      <slot></slot>
+      <FooterArea></FooterArea>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderArea from './HeaderArea';
+import FooterArea from './FooterArea';
+
+export default {
+  components: {
+    HeaderArea,
+    FooterArea
+  }
+}
+</script>
