7de486392a320efe9696bc2543d129ece41657f8
hegdeashwin <ashwin.hegde3@gmail.com>
2016-10-14 22:28:51 +0530

    Re-factors router integration

--- src/app/chief-engine.js
+++ src/app/chief-engine.js
@@ -1,21 +1,6 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-
+import Router from './router/Router';
 import MasterLayout from './views/Layout/Layout';
-import Dashboard from './views/Dashboard/Dashboard';
-import Settings from './views/Settings/Settings';
-
-Vue.use(VueRouter);
-
-const Router = new VueRouter({
-  routes: [{
-    path: '/',
-    component: Dashboard
-  }, {
-    path: '/settings',
-    component: Settings
-  }]
-});
 
 /* eslint-disable no-new */
 new Vue(Vue.util.extend({
