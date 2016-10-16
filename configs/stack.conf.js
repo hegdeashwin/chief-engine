b24660fc99eb652c7bd23b6b0fc9952289f736f8
hegdeashwin <ashwin.hegde3@gmail.com>
2016-10-16 23:25:50 +0530

    Adds missing stack config file

---
+++ configs/stack.conf.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+	"server": {
+		"host": "localhost",
+		"port": 9000,
+		"address": "localhost"
+	},
+	"client": {
+		"staticServe": "dist",
+		"listing": true,
+		"index": true,
+		"showHidden": true
+	}
+};
